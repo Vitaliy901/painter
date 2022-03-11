1956c3be4734950d09cc94bcc08dbaa94985813a
Vitaliy901 <allanamos90@gmail.com>
2022-03-11 19:50:35 +0200

    Initialization

---
+++ js/main.js
@@ -0,0 +1,72 @@
+let main = document.querySelector('main')
+let board = document.querySelector('.board')
+let menu = document.querySelector('.menu')
+let range = document.querySelector('[name=range]')
+let hue = document.querySelector('.hue')
+let rubber = document.querySelector('.rubber')
+let canvas = document.querySelector('canvas')
+let context = canvas.getContext('2d')
+
+function getRadians(degrees) {
+	return (Math.PI / 180) * degrees;
+}
+
+let bool = false;
+let xv = 0;
+let yv = 0;
+let rangeV = range.value = 20;
+let rubberFlag = false;
+// ======================canvas====================
+range.addEventListener('change', function (e) {
+	rangeV = range.value
+})
+canvas.addEventListener('mouseout', function (e) {
+	let mouseout = new Event('mouseup')
+	this.dispatchEvent(mouseout)
+})
+
+canvas.addEventListener('mousedown', function (e) {
+	bool = !bool;
+	xv = e.pageX - (Number.parseInt(getComputedStyle(canvas).marginLeft) + Number.parseInt(getComputedStyle(main).marginLeft));
+	yv = e.pageY - (this.offsetTop + Number.parseInt(getComputedStyle(canvas).borderTop));
+	if (bool) {
+		if (rubberFlag) {
+			console.log('!')
+			context.beginPath()
+			context.clearRect(xv - rangeV / 2,yv - rangeV / 2,rangeV,rangeV)
+		} else {
+			context.arc(xv,yv,rangeV,0,getRadians(360))
+			context.beginPath()
+			context.arc(xv,yv,rangeV,0,getRadians(360))
+		}
+	}
+	context.fillStyle = 'green'
+	context.fill()
+})
+canvas.addEventListener('mouseup', function (e) {
+	bool = false;
+})
+canvas.addEventListener('mousemove', function (e) {
+
+	let x = e.pageX - (Number.parseInt(getComputedStyle(canvas).marginLeft) + Number.parseInt(getComputedStyle(main).marginLeft));
+	let y = e.pageY - (this.offsetTop + Number.parseInt(getComputedStyle(canvas).borderTop));
+	rubber.textContent = x + ':' + y
+	if (bool) {
+		if (rubberFlag) {
+			console.log('!')
+			context.beginPath()
+			context.clearRect(x - 10,y - 10,rangeV,rangeV)
+		} else {
+			context.arc(x,y,rangeV,0,getRadians(360))
+			context.beginPath()
+			context.arc(x,y,rangeV,0,getRadians(360))
+		}	
+	}
+	context.fillStyle = 'green'
+	context.fill()
+})
+// ===============rubber===========
+rubber.addEventListener('click', function (e) {
+	rubberFlag = !rubberFlag;
+	console.log(rubberFlag )
+})
